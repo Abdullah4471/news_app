0e4e41d50cacd05f17a39bdcbe6a456e80f8b720
Abdullah4471 <Abdullah4471>
2023-05-31 15:38:59 +0500

    first commit

---
+++ lib/consts/api_consts.dart
@@ -0,0 +1,5 @@
+const String BASEURL = "newsapi.org";
+const String API_KEY = "c6a64251e05d41c188d51dacc0b7d960"; //84a442d2ffca4e85832a4b68696ee890
+
+// Be careful use your link
+const String BASEURL_FIREBASE = "news-app-67cc6-default-rtdb.firebaseio.com";
---
+++ lib/inner_screens/bookmarks_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:news_app/widgets/drawer_widget.dart';
+
+import '../services/utils.dart';
+import '../widgets/empty_screen.dart';
+
+class BookmarkScreen extends StatefulWidget {
+  const BookmarkScreen({Key? key}) : super(key: key);
+
+  @override
+  State<BookmarkScreen> createState() => _BookmarkScreenState();
+}
+
+class _BookmarkScreenState extends State<BookmarkScreen> {
+  @override
+  Widget build(BuildContext context) {
+    // Size size = Utils(context).getScreenSize;
+    final Color color = Utils(context).getColor;
+    return Scaffold(
+      drawer: DrawerWidget(),
+        appBar: AppBar(
+          iconTheme: IconThemeData(color: color),
+          elevation: 0,
+          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+          centerTitle: true,
+          title: Text(
+            'Bookmarks',
+            style: GoogleFonts.lobster(
+                textStyle:
+                    TextStyle(color: color, fontSize: 20, letterSpacing: 0.6)),
+          ),
+        ),
+        body:  EmptyNewsWidget(
+          text: 'You didn\'t add anything yet to your bookmarks',
+          imagePath: "assets/images/bookmark.png",
+        )
+        // ListView.builder(
+        //     itemCount: 20,
+        //     itemBuilder: (ctx, index) {
+        //       return const ArticlesWidget();
+        //     }),
+        );
+  }
+}
